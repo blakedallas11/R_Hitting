37a35bbdf4d8f89bdbe52cd8886a0e739ca11f4b
Blake Dallas <blakedallas36@gmail.com>
2021-12-16 15:18:35 -0600

    Updated Ideas.txt and learned to join two tables with an inner join to get correlating data to connect in a new table and query that.

--- pitch_Double_Queries.R
+++ pitch_Double_Queries.R
@@ -1,4 +1,84 @@
 
+EVbyPitchTypeWPrevPitch = dbGetQuery(db, "SELECT previous_pitch, pitch_type, ROUND(AVG(launch_speed),1) AS 'AVG_EV_W_PREV', ROUND(AVG(launch_angle),1) AS 'AVG_LA_W_PREV'
+                           FROM ordered_w_prev_pitch
+                           WHERE description == 'hit_into_play'
+                           AND previous_pitch != '<NA>'
+                           AND pitch_type != '<NA>'
+                           AND pitch_type != 'CS'
+                           AND pitch_type != 'FA'
+                           AND pitch_type != 'KN'
+                           AND pitch_type != 'EP'
+                           AND previous_pitch != 'CS'
+                           AND previous_pitch != 'FA'
+                           AND previous_pitch != 'KN'
+                           AND previous_pitch != 'EP'
+                           GROUP BY pitch_type, previous_pitch")
+print(EVbyPitchTypeWPrevPitch)
+
+dbWriteTable(db, "EVbyPrevPitchPitchType", EVbyPitchTypeWPrevPitch,overwrite = T, row.names = F)
+
+EVbyPitchType = dbGetQuery(db, "SELECT pitch_type, ROUND(AVG(launch_speed),1) AS 'AVG_EV', ROUND(AVG(launch_angle),1) AS 'AVG_LA'
+                           FROM ordered_w_prev_pitch
+                           WHERE description == 'hit_into_play'
+                           AND pitch_type != '<NA>'
+                           AND pitch_type != 'CS'
+                           AND pitch_type != 'FA'
+                           AND pitch_type != 'KN'
+                           AND pitch_type != 'EP'
+                           GROUP BY pitch_type")
+print(EVbyPitchType)
+
+dbWriteTable(db, "EVbyPitchType", EVbyPitchType, overwrite = T, row.names = F)
+
+
+#Gets prev pitch, curr pitch, avg ev with prev pitch, avg ev of curr pitch and the difference between the pitch ev and sequenced
+#ev. This can help us understand pitch sequencing and it's effect on how hard the ball is hit.
+EVtwoColumns = dbGetQuery(db, "SELECT  EVbyPrevPitchPitchType.previous_pitch, EVbyPrevPitchPitchType.pitch_type, EVbyPrevPitchPitchType.AVG_EV_W_PREV,
+                          EVbyPitchType.AVG_EV, EVbyPrevPitchPitchType.AVG_EV_W_PREV - EVbyPitchType.AVG_EV AS 'difference'
+                          FROM EVbyPrevPitchPitchType 
+                          INNER JOIN EVbyPitchType ON EVbyPrevPitchPitchType.pitch_type = EVbyPitchType.pitch_type
+                          ORDER BY difference ASC")
+print(EVtwoColumns)
+
+
+
+#Plot 1
+#####
+
+EVandLA = dbGetQuery(db, "SELECT  EVbyPrevPitchPitchType.previous_pitch, EVbyPrevPitchPitchType.pitch_type, EVbyPrevPitchPitchType.AVG_EV_W_PREV,
+                          EVbyPitchType.AVG_EV, EVbyPrevPitchPitchType.AVG_EV_W_PREV - EVbyPitchType.AVG_EV AS 'EV_difference', 
+                          EVbyPrevPitchPitchType.AVG_LA_W_PREV,
+                          EVbyPitchType.AVG_LA, EVbyPrevPitchPitchType.AVG_LA_W_PREV - EVbyPitchType.AVG_LA AS 'LA_difference'
+                          FROM EVbyPrevPitchPitchType 
+                          INNER JOIN EVbyPitchType ON EVbyPrevPitchPitchType.pitch_type = EVbyPitchType.pitch_type
+                          ORDER BY EV_difference ASC")
+print(EVandLA)
+
+#This is how you print a certain column of an R dataframe
+print(EVandLA$previous_pitch)
+
+#Set the outlier range to take out the top two changes in launch angle that were down in the -30 and lower range and messing
+# with the plot
+yout = EVandLA$LA_difference < -10
+
+# the function that calculates the linear regression for the trend line on the plot
+linearRegressionLine = lm(EVandLA$LA_difference[!yout] ~ EVandLA$EV_difference[!yout], EVandLA)
+
+#Plot the two axes from the data frame created by the query above
+plot(EVandLA$EV_difference[!yout],EVandLA$LA_difference[!yout],type = "o")
+
+#put the trendline on the plot
+lines(EVandLA$EV_difference[!yout],predict(linearRegressionLine))
+
+#print the y intercept and the slope of the trend line
+print(coef(linearRegressionLine))
+
+
+
+
+
+
+######
 
 
 
@@ -79,8 +159,3 @@ pitchSequenceHitCountsOnly = dbGetQuery(db, "SELECT previous_pitch, pitch_type, 
                                          GROUP BY pitch_type, previous_pitch")
 print(pitchSequenceHitCountsOnly)
 
-
-
-pitchSequenceHitCountsOnly = rbind(pitchSequenceHitCountsOnly,12,13,10)
-BABIP_pitchSequence = pitchSequenceHitCountsOnly / pitchSequenceBIPCountsOnly
-print(BABIP_pitchSequence)
